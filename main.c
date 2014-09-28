b48e6cb14d0a8885ac019e3019bf125cb5b5be96
Helder Rodrigues <helder.garjunior@outlook.com>
2014-09-28 12:51:50 -0300

    Basic changes

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pilha.h"
+/**
+    Developed by Helder Rodrigues.
+    Reports to helder.garjunior@outlook.com
+**/
 
 int main()
 {
     pilha* p = alocar_pilha();
 
     /**
-        Monte seu cenario de teste aqui.
-        Utilize as funcoes prontas.
+        Build your tests here.
+        Use the ready functions.
+
+        Functions:
+        inserir_pilha;
+        remover_pilha;
+        alocar_pilha;
+        destruir_pilha;
+        imprimir_pilha;
+
     **/
 
     return 0;
